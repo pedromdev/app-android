d95d4a09952b8fc462b7c082ceb9f56ebb4ecffc
Pedro Alves <pedromarcelodesaalves@gmail.com>
2017-05-10 20:37:56 -0300

    Update ListaAlunosActivity.java

--- app/src/main/java/com/example/pedromdev/listapplication/ListaAlunosActivity.java
+++ app/src/main/java/com/example/pedromdev/listapplication/ListaAlunosActivity.java
@@ -9,7 +9,6 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.inputmethod.EditorInfo;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
